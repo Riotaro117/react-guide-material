d7104f50d628e4a8d0c10408ac800ad038f92c08
riotaro <rugby117117@gmail.com>
2025-11-10 06:22:13 +0900

    propsの受け渡しについて コンポーネントの分割

--- 04_react_basic/src/090_props/start/Example.jsx
+++ 04_react_basic/src/090_props/start/Example.jsx
@@ -1,5 +1,18 @@
-import Child from "./components/Child";
+import Child from './components/Child';
 
-const Example = () => <Child />;
+const Example = () => {
+  const hello = (arg) => `Hello ${arg}`;
+  // propsで渡す値はオブジェクトになる。ここで言うとcolor:"red";
+  // propsを子要素で指定した場合、親要素で指定しないとエラーになる
+
+  const o = { color: 'red', num: 123 };
+  return (
+    // スプレッド演算子を使ってまとめて記述することもできる
+    <>
+      <Child {...o} fn={hello} bool obj={{ name: 'Tom', age: 18 }} />
+      {/* <Child color="red" num fn={hello} obj /> */}
+    </>
+  );
+};
 
 export default Example;
--- 04_react_basic/src/090_props/start/components/Child.jsx
+++ 04_react_basic/src/090_props/start/components/Child.jsx
@@ -3,12 +3,29 @@
 文：変数宣言、for文、if文、switch文やセミコロンで区切るもの。
 */
 
-import "./Child.css";
+import './Child.css';
 
-const Child = () => {
+// const Child = (props) => {
+//   console.log(props);
+//   return (
+//     // props.colorはオブジェクトのキーを指定している color:"red";
+//     <div className={`component ${props.color}`}>
+//       <h3>Hello Component</h3>
+//     </div>
+//   );
+// };
+
+// propsはオブジェクトなので分割代入でもできる
+const Child = ({ color = 'green', num, fn, bool, obj }) => {
+  // console.log({ fn });
   return (
-    <div className="component">
+    // props.colorはオブジェクトのキーを指定している color:"red";
+    <div className={`component ${color}`}>
       <h3>Hello Component</h3>
+      <h3>{num}</h3>
+      <h3>{fn('Props')}</h3>
+      <h3>{bool ? 'true' : 'false'}</h3>
+      <h3>{obj.name + obj.age}</h3>
     </div>
   );
 };
