71d1fcb5b8619bf4c6888d11d42161af5db385e2
riotaro <rugby117117@gmail.com>
2025-11-13 06:25:00 +0900

    オブジェクト型の更新方法 useState

--- 05_state_and_event/src/050_prev_state/start/Example.jsx
+++ 05_state_and_event/src/050_prev_state/start/Example.jsx
@@ -1,7 +1,27 @@
-import { useState } from "react";
+import { useState } from 'react';
 
+// カウントする時の変数名はcountにする
 const Example = () => {
-  
+  const [count, setCount] = useState(0);
+
+  // 更新関数の引数に値の変化を入れる
+  const countUp = () => {
+    setCount(count + 1);
+    setCount((prev) => prev + 1); //前の値を続けて処理を追加したい時は、引数に前の値を入れた関数を書くこと
+    console.log(count); //この時点でcount = 0;再レンダリングされた時に更新される非同期処理
+  };
+
+  const countDown = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <>
+      <p>現在のカウント数: {count}</p>
+      <button onClick={countUp}>+</button>
+      <button onClick={countDown}>-</button>
+    </>
+  );
 };
 
 export default Example;
--- 05_state_and_event/src/060_state_object/start/Example.jsx
+++ 05_state_and_event/src/060_state_object/start/Example.jsx
@@ -1,7 +1,39 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const Example = () => {
-  const personObj = { name: "Tom", age: 18 };
+  const personObj = { name: 'Tom', age: 18 };
+  // useStateの初期値には、オブジェクトを入れる
+  const [person, setPerson] = useState(personObj);
+
+  // オブジェクトを更新するときは、オブジェクトを詳細に書く
+  // setPersonにはオブジェクトを入れて、更新したいプロパティにのみ変化を加える。後はそのまま記述
+  const changeName = (e) => {
+    // setPerson({ name: e.target.value, age: person.age });
+
+    // スプレッド演算子も使用できる
+    setPerson({ ...person, name: e.target.value });
+  };
+
+  const changeAge = (e) => {
+    setPerson({ name: person.name, age: e.target.value });
+  };
+
+  const reset = () => {
+    setPerson({ name: '', age: '' });
+  };
+
+  // inputタグのvalueが初期値を取る。placeholderは見えるだけ
+  return (
+    <>
+      <h3>Name:{person.name}</h3>
+      <h3>Age:{person.age}</h3>
+      <input type="text" onChange={changeName} value={person.name} />
+      <input type="number" onChange={changeAge} value={person.age} />
+      <div>
+        <button onClick={reset}>リセット</button>
+      </div>
+    </>
+  );
 };
 
 export default Example;
