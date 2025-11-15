094cdda1ed2e1f53e670e6ea8c78d21d95f46a9f
riotaro <rugby117117@gmail.com>
2025-11-15 22:01:17 +0900

    配列の表示

--- 06_control_and_form/src/010_list_components/start/Example.jsx
+++ 06_control_and_form/src/010_list_components/start/Example.jsx
@@ -1,13 +1,22 @@
-
-const animals = ["Dog", "Cat", "Rat"];
+const animals = ['Dog', 'Cat', 'Rat'];
 
 const Example = () => {
+  // 新しく配列を定義する
+  const animalList = [];
+  // for of文を使って配列を一つずつ取り出し、空の配列に入れていく
+  for (const animal of animals) {
+    animalList.push(<li>Hello, {animal}</li>);
+  }
 
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {/* 毎回配列を取り出すのは冗長 */}
+        {/* <li>Hello, {animals[0]}</li>
+        <li>Hello, {animals[1]}</li>
+        <li>Hello, {animals[2]}</li> */}
+        {animalList}
       </ul>
     </>
   );
